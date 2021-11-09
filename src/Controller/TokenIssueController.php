e35fc13754f386702a2f513da756238d30a04b60
Abdul Hafidz Anshari <aditans88@gmail.com>
2021-11-09 20:10:46 +0700

    fix error controller

--- src/Controller/TokenIssueController.php
+++ src/Controller/TokenIssueController.php
@@ -2,7 +2,10 @@
 
 namespace OAuthServer\Controller;
 
+use Psr\Container\ContainerInterface;
 use Hyperf\HttpServer\Annotation\Controller;
+use League\OAuth2\Server\AuthorizationServer;
+use OAuthServer\Repositories\TokenRepository;
 use Hyperf\HttpServer\Annotation\RequestMapping;
 use Hyperf\HttpServer\Contract\RequestInterface;
 use Hyperf\HttpServer\Contract\ResponseInterface;
@@ -15,6 +18,20 @@ use League\OAuth2\Server\Exception\OAuthServerException;
  */
 class TokenIssueController
 {
+    protected $container;
+    protected $server;
+    protected $tokens;
+
+    public function __construct(
+        ContainerInterface $container,
+        AuthorizationServer $server,
+        TokenRepository $tokens
+    ) {
+        $this->container = $container;
+        $this->server    = $server;
+        $this->tokens    = $tokens;
+    }
+
     /**
      * @RequestMapping(path="/oauth/token", methods="post")
      */
