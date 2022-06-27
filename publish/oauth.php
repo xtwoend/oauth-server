bdffab590e2e3cda95eca69c4d591adcf9b690a4
Abdul Hafidz A <aditans88@gmail.com>
2022-06-27 16:01:31 +0700

    date interval

--- publish/oauth.php
+++ publish/oauth.php
@@ -3,8 +3,9 @@
 return [
     // config here
     'key' => 'CpmLVtjV8diGbhEsVD3IWoVOn31pRpmupEcxMCgtXp9LGpe39F',
+    // return DateInterval https://www.php.net/manual/en/class.dateinterval.php
     'expire_in' => [
-        'token' =>  'P1D', // DateInterval
+        'token' =>  'P1D', 
         'refresh_token' => 'P1M',
         'personal_token' => 'P1M'
     ],
